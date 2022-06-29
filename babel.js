9ef31c330768614debff28608dcfbb716d33216c
NdekoCode <arickbulakali@gmail.com>
2022-06-29 16:45:32 +0200

    add babel true configuration and output file watches

---
+++ babel.js
@@ -0,0 +1,3 @@
+const variable = 5;
+let str = "LOl World";
+console.log(variable);
---
+++ script-compiled.js
@@ -0,0 +1,5 @@
+const variable = 5;
+let str = "LOl World";
+console.log(variable);
+
+//# sourceMappingURL=script-compiled.js.map
