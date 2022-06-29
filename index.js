9a6fa44bebe49d68ddb36989db1bc98762aec674
NdekoCode <arickbulakali@gmail.com>
2022-06-29 16:54:49 +0200

    template string in js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const myName = "Arick";
 let age = 5;
 
 if (age < 18) {
-    console.log("Mineur")
+  console.log("Mineur");
 } else if (age > 18 && age < 25) {
-    console.log("Majeur");
+  console.log("Majeur");
 } else if (age < 50 && age > 25) {
-    console.log("Adulte");
+  console.log("Adulte");
 } else {
-    console.log("Trop vieu");
+  console.log("Trop vieu");
 }
 const x = BigInt(Number.MAX_SAFE_INTEGER); // 9007199254740991n
 console.log(x + 1n === x + 2n); // 9007199254740992n === 9007199254740993n Va afficher => false
@@ -17,3 +17,9 @@ console.log(x + 1n === x + 2n); // 9007199254740992n === 9007199254740993n Va af
 
 Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2; // 9007199254740992 === 9007199254740992 Va afficher => true
 console.log(Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2);
+
+const username = "Ndekocode";
+
+console.log(
+  `Mon nom d'utilisateur sur github ${username} est un nom congolais`
+);
