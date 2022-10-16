d56c1c4110e632c965247fe261dc67f4447fe809
NdekoCode <arickbulakali@gmail.com>
2022-10-16 08:58:06 +0200

    init function

--- grafikart/index.js
+++ grafikart/index.js
@@ -6,15 +6,17 @@
 
 
 */
-const year = 2022;
-const birthYear = parseInt(prompt("Entrer votre année de naissance !"));
-const age = year - birthYear;
-if (age <= 13) {
-  alert("Suivez lilo et steach");
-} else if (age <= 18) {
-  alert("Suivez le film The matrix");
-} else {
-  alert("Suivez Devil Dead");
+function viewCinema() {
+  const year = 2022;
+  const birthYear = parseInt(prompt("Entrer votre année de naissance !"));
+  const age = year - birthYear;
+  if (age <= 13) {
+    alert("Suivez lilo et steach");
+  } else if (age <= 18) {
+    alert("Suivez le film The matrix");
+  } else {
+    alert("Suivez Devil Dead");
+  }
 }
 
 // Deux nombre
