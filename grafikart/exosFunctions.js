bb885f3e9afa2bfe885b100566e0213828b8b60a
NdekoCode <arickbulakali@gmail.com>
2022-10-20 14:29:30 +0200

    add moyenne student in function exercices

--- grafikart/exosFunctions.js
+++ grafikart/exosFunctions.js
@@ -1,8 +1,7 @@
 // is Palendromme
 function isPalendrome(value) {
-  return (
-    value.split("").reverse().join("").toUpperCase() === value.toUpperCase()
-  );
+  const str = value.split("").reverse().join("");
+  return str.toUpperCase() === value.toUpperCase();
 }
 const words = {
   kayak: true,
@@ -11,4 +10,40 @@ const words = {
   oui: false,
   Bonjour: false,
 };
-console.log(isPalendrome("Cool"));
+/* 
+for (let index in words) {
+  console.log(isPalendrome(index));
+} */
+const students = [
+  {
+    name: "John",
+    notes: [1, 20, 18, 19, 12],
+  },
+  {
+    name: "Jane",
+    notes: [17, 18, 20, 13, 15],
+  },
+  {
+    name: "Sophie",
+    notes: [17, 12, 14, 15, 13],
+  },
+  {
+    name: "Marc",
+    notes: [2, 3, 5, 8, 9],
+  },
+  {
+    name: "Manon",
+    notes: [18, 17, 18, 19, 12],
+  },
+];
+function moyenne(notes) {
+  return notes.reduce((acc, val) => acc + val) / notes.length;
+}
+for (let student of students) {
+  student.moyenne = moyenne(student.notes);
+}
+function compareStudent(a, b) {
+  return b.moyenne - a.moyenne;
+}
+students.sort(compareStudent);
+console.log(students);
