5eef58ef08fa46bc4493638290e4556e4cc95d70
NdekoCode <arickbulakali@gmail.com>
2022-10-22 09:31:52 +0200

    learn: Date and date methods and function

--- grafikart/date.js
+++ grafikart/date.js
@@ -4,19 +4,18 @@ function addDays(date, days) {
   newDate.setDate(newDate.getDate() + days);
   return newDate;
 }
-const YEARS = "years";
-const MONTHS = "months";
-const WEEKS = "weeks";
-const DAYS = "days";
-const HOURS = "hours";
 
 /**
- *
+ * Ajoute ou retire un interval à une date
  * @param {Date} date La date
  * @param {number} n
  * @param {string} unit
  */
 function addIntervalInDate(date, n, unit) {
+  const YEARS = "years";
+  const MONTHS = "months";
+  const DAYS = "days";
+  const HOURS = "hours";
   const newDate = new Date(date.getTime());
   switch (unit) {
     case HOURS:
@@ -34,9 +33,49 @@ function addIntervalInDate(date, n, unit) {
   }
   return newDate;
 }
+
+const YEARS = 0;
+const MONTHS = 1;
+const DAYS = 2;
+const HOURS = 3;
+const MINUTES = 4;
+const SECONDS = 4;
+/**
+ * Ajoute un interval à une date
+ * @param {Date} date L'objet date de débart
+ * @param  {Object<{years:number,months:number,days:number,hours:number,minutes:number,seconds:number}>} interval Les intervals  des elements de la date à supprimer
+ * @returns
+ */
+function addInterval(date, interval) {
+  const intervalUnit = {
+    years: 0,
+    months: 1,
+    days: 2,
+    hours: 3,
+    minutes: 4,
+    seconds: 5,
+    milliseconds: 6,
+  };
+
+  const partsDate = [
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds(),
+  ];
+  for (let [unit, value] of Object.entries(interval)) {
+    if (unit in intervalUnit) {
+      partsDate[intervalUnit[unit]] += value;
+    }
+  }
+  return new Date(...partsDate);
+}
 const today = new Date();
 const tomorrow = addDays(today, 1);
-const future = addIntervalInDate(today, 1, YEARS);
+const future = addInterval(today);
 // console.log(today.toLocaleString());
 // console.log(tomorrow.toLocaleString());
 console.log(future);
